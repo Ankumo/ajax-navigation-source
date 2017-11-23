e0e14bac07b7e6a04c789f0edd989f41a8229372
Vyacheslav <ankubear@gmail.com>
2017-11-23 14:55:53 +0800

    i'm dumb :>
    addEventListener doesn't support multiple events

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $('a[href]:not([class="away-link"])').click(function (e) {
     history.pushState(null, null, $(this).attr('href'));
 });
 
-window.addEventListener("popstate load", function (e) {
+$(window).on("popstate load", function (e) {
     getContent(location.pathname);
 });
 
